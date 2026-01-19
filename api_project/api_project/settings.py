95d5876a86c9239c68afe0173c3bb1ca2b3acc7c
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2026-01-19 13:12:14 +0100

    feat:configured project  for deployment

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!pp&epm=&$b4e41luhz4q4tb_+hf6!=46n$6#e0#b=g7$=o#_("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
